1eb4a6fcb6daf04bc7bc970df0f9221714359557
Hi-Ji <zj010110@gmail.com>
2023-03-30 00:57:46 +1300

    Delete useless

--- src/App.js
+++ src/App.js
@@ -22,63 +22,14 @@ const App = () => {
     <div>
       <Router>
         <Header handleChangeInput={(e)=>handleChangeInput(e)} />
-        
-
         {/* <Test /> */}
-
-
         <Routes>
           <Route path="/" element={<Login  />} />
           <Route path="/products" element={<Products search={input} />} />
         </Routes>
-        
-        
       </Router>
-      
     </div>
   )
 }
 
 export default App
-
-
-
-// import React, { useState } from "react";
-
-// function Table() {
-//   const [rows, setRows] = useState([
-//     { id: 2, name: "John" },
-//     { id: 1, name: "Mary" },
-//     { id: 3, name: "Bob" },
-//   ]);
-
-//   function handleClick() {
-//     const sortedRows = [...rows].sort((a, b) => a.id - b.id);
-//     setRows(sortedRows);
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={handleClick}>Sort Rows</button>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>Name</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {rows.map((row) => (
-//             <tr key={row.id}>
-//               <td>{row.id}</td>
-//               <td>{row.name}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default Table;
-
